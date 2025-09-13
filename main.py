3a9a1261b55e3b7bca954cc0c19dfaace05a92f6
mray760 <94726783+mray760@users.noreply.github.com>
2025-09-13 08:03:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from je_tran import create_journal_from_transactions
 from income_statement import create_income_statement
+from operating_cf import create_op_cf
 from load_excel import load_tran_data, load_yardi_data
 from yardi_norm import normalize_raw_yardi
 
-tran_filepath = '/Users/mattray/Desktop/GV Accouting/Inputs/Production/gv_tran_prod.xlsx'
-yardi_filepath = '/Users/mattray/Desktop/GV Accouting/Inputs/Test/yardi_tran_test.xlsx'
+tran_filepath = '/Users/mattray/Desktop/GV Accouting/Inputs/Production/transactions/8.25_transactions.xlsx'
+yardi_filepath = '/Users/mattray/Desktop/GV Accouting/Inputs/Production/transactions/8.25_yardi_tran.xlsx'
+cash_bal_filepath = '/Users/mattray/Desktop/GV Accouting/Inputs/Production/cash_balances/cash_balances.xlsx'
 
 from_period = '08-2025'
 to_period = '08-2025'
@@ -34,7 +36,7 @@ def create_gl_ledgers(general_journal):
 
 # Trial Balance
 def create_trial_balance(general_journal):
-    tb = general_journal.groupby(['gl_code','account']).agg(
+    tb = general_journal.groupby(['period','gl_code','account']).agg(
         total_debit=('debit', 'sum'),
         total_credit=('credit', 'sum')
     ).reset_index()
@@ -42,12 +44,13 @@ def create_trial_balance(general_journal):
     return tb
 
 # Save to Excel
-def save_to_excel(general_journal, gl_ledgers, trial_balance, income_statement, output_file):
+def save_to_excel(general_journal, gl_ledgers, trial_balance, income_statement, operating_cf, output_file):
     with pd.ExcelWriter(output_file) as writer:
         general_journal.to_excel(writer, sheet_name='General Journal', index=False)
         gl_ledgers.to_excel(writer, sheet_name='GL Ledgers', index=False)
         trial_balance.to_excel(writer, sheet_name='Trial Balance', index=False)
         income_statement.to_excel(writer,sheet_name='Income Statement',index=False)
+        operating_cf.to_excel(writer,sheet_name='Operating CF', index=False)
 
 # Run full accounting pipeline
 def run_accounting_pipeline(excel_input, yardi_input, output_file):
@@ -57,7 +60,9 @@ def run_accounting_pipeline(excel_input, yardi_input, output_file):
     gl_ledgers = create_gl_ledgers(general_journal)
     trial_balance = create_trial_balance(general_journal)
     income_statement = create_income_statement(trial_balance)
-    save_to_excel(general_journal, gl_ledgers, trial_balance, income_statement, output_file)
+    operating_cf = create_op_cf(trial_balance=trial_balance,income_statement=income_statement)
+    save_to_excel(general_journal, gl_ledgers, trial_balance, income_statement, operating_cf, output_file)
+
 
 # Example run:
 run_accounting_pipeline(excel_input = tran_filepath, yardi_input= yardi_filepath,output_file = '/Users/mattray/Desktop/GV Accouting/Outputs/accounting_output_5.xlsx')
