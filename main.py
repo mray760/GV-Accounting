8276c12ac121914aa97471d2cd4b4147f9b94b6b
mray760 <94726783+mray760@users.noreply.github.com>
2025-09-13 19:42:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from je_tran import create_journal_from_transactions
 from income_statement import create_income_statement
-from operating_cf import create_op_cf
-from load_excel import load_tran_data, load_yardi_data
+from operating_cf import create_cf_statement
+from load_excel import load_tran_data, load_yardi_data, load_cash_balances
 from yardi_norm import normalize_raw_yardi
 
 tran_filepath = '/Users/mattray/Desktop/GV Accouting/Inputs/Production/transactions/8.25_transactions.xlsx'
 yardi_filepath = '/Users/mattray/Desktop/GV Accouting/Inputs/Production/transactions/8.25_yardi_tran.xlsx'
 cash_bal_filepath = '/Users/mattray/Desktop/GV Accouting/Inputs/Production/cash_balances/cash_balances.xlsx'
 
-from_period = '08-2025'
-to_period = '08-2025'
+from_period = '08/2025'
+to_period = '08/2025'
 
 # Combine tenant and expense entries into one journal
 def create_general_journal(yardi_df, transactions_df):
@@ -56,11 +56,12 @@ def save_to_excel(general_journal, gl_ledgers, trial_balance, income_statement, 
 def run_accounting_pipeline(excel_input, yardi_input, output_file):
     transactions_df = load_tran_data(excel_input)
     yardi_df = load_yardi_data(yardi_input)
+    beg_cash_bal = load_cash_balances(cash_bal_filepath,from_period)
     general_journal = create_general_journal(yardi_df, transactions_df)
     gl_ledgers = create_gl_ledgers(general_journal)
     trial_balance = create_trial_balance(general_journal)
     income_statement = create_income_statement(trial_balance)
-    operating_cf = create_op_cf(trial_balance=trial_balance,income_statement=income_statement)
+    operating_cf = create_cf_statement(trial_balance=trial_balance,income_statement=income_statement,cash_balances=beg_cash_bal)
     save_to_excel(general_journal, gl_ledgers, trial_balance, income_statement, operating_cf, output_file)
 
 
